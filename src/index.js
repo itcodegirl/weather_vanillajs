51844ca11af6c5270045c06466203128013044b1
Jenna <88835851+itcodegirl@users.noreply.github.com>
2024-05-16 15:24:15 -0500

    Update index.js
    API Documentation to include key and url

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ require('dotenv').config();
 console.log(process.env.apiKey);
 
 function searchCity(city) {
+	//make a api call ans update user interface
 	let apiKey = '';
+	let apiURL = 'https://api.shecodes.io/weather/v1/current?query={query}&key={key}';
 
 	console.log(apiKey);
 }
@@ -13,7 +15,8 @@ function handleSearchSubmit(event) {
 
 	let searchInput = document.querySelector('#search-form-input');
 	let cityElement = document.querySelector('#city');
-	cityElement.innerHTML = searchInput.ariaValueMax;
+	cityElement.innerHTML = searchInput.value;
+	searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector('#search-form');
