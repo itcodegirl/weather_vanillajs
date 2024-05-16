3234c45394aeb80bf5f7eeabd5e6a2ed8961ae23
Jenna <88835851+itcodegirl@users.noreply.github.com>
2024-05-16 16:04:01 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
-require('dotenv').config();
+function refreshWeather(response) {
+	let temperatureElement = document.querySelector("#temperature");
+	let temperature = response.data.temperature.current;
+	let cityElement = document.querySelector("#city");
 
-console.log(process.env.apiKey);
+	cityElement.innerHTML = response.data.city;
+	temperatureElement.innerHTML = Math.round(temperature);
+}
 
 function searchCity(city) {
-	//make a api call ans update user interface
-	let apiKey = '';
-	let apiURL = 'https://api.shecodes.io/weather/v1/current?query={query}&key={key}';
-
-	console.log(apiKey);
+	let apiKey = "b2a5adcct04b33178913oc335f405433";
+	let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+	axios.get(apiUrl).then(refreshWeather);
 }
 
 function handleSearchSubmit(event) {
 	event.preventDefault();
+	let searchInput = document.querySelector("#search-form-input");
 
-	let searchInput = document.querySelector('#search-form-input');
-	let cityElement = document.querySelector('#city');
-	cityElement.innerHTML = searchInput.value;
 	searchCity(searchInput.value);
 }
 
-let searchFormElement = document.querySelector('#search-form');
-searchFormElement.addEventListener('submit', handleSearchSubmit);
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Paris");
